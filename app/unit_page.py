8002b67d4b3c863b03710f73fb43adda145a422e
ahmadshaqra <ahmad.shaqra1@gmail.com>
2025-06-07 23:26:21 +1000

    Set up unit page and unit data class.

--- app/unit_page.py
+++ app/unit_page.py
@@ -1,7 +1,35 @@
+"""
+    unit_page.py
+
+    Contains the unit page logic.
+"""
+
 import tkinter as tk
+from tkinter import ttk, messagebox
+from data.unit import Unit
+
 class UnitPage(tk.Frame):
+    """
+        Manages the unit page.
+    """
+
     def __init__(self, root: tk.Frame, main_window: tk.Tk) -> None:
+        """
+            Initialises the unit page.
+
+            Args:
+                root (tk.Frame): the main contents frame.
+                main_window (tk.Tk): the main window.
+        """
+
+        # initialises the frame and unit data
         super().__init__(root)
-        tk.Label(self, text="UNIT PAGE HERE").pack()
+        self.unit = Unit()
+        self.main_window = main_window
+
     def load_page(self) -> None:
+        """
+            Loads page from data.
+        """
+
         pass
---
+++ data/unit.py
@@ -0,0 +1,19 @@
+"""
+    unit.py
+
+    Contains the unit class.
+"""
+
+from utils.file_manager import FileManager
+
+class Unit:
+    """
+        Stores and manages the unit information of the student.
+    """
+
+    def __init__(self) -> None:
+        """
+            Initialises the unit data from file(s).
+        """
+
+        pass
